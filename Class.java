71c528f48b3cc4c1119331b9a7f7bf1fc525a9ca
Mikhail <LevinMK23@gmail.com>
2018-10-10 23:50:08 +0300

    Create Class.java

---
+++ Class.java
@@ -0,0 +1,12 @@
+
+import java.util.ArrayList;
+
+public class Class {
+    String name;
+    ArrayList<Pupil> pupils;
+
+    public Class(String name) {
+        this.name = name;
+        pupils = new ArrayList<>();
+    }
+}
